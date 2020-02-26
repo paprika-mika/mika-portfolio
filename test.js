47c9f31b81701e7b74af8763c6c6bd510591b8f6
PaprikaMika <55081913+PaprikaMika@users.noreply.github.com>
2020-02-26 12:00:39 +0100

    working code, no final content

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $(".image").click(function(){
+  $(".image, .cross-reverse, .menu-button").click(function(){
     $("#start").removeClass("pop-up").addClass("pop-up-gone");
     $("#halfrow").removeClass("row").addClass("row-half");
     $("#halfcolumn1, #halfcolumn2, #halfcolumn3, #halfcolumn4").removeClass("column").addClass("column-half");
@@ -19,14 +19,30 @@ $(document).ready(function(){
 
 
 $( function() {
-
       $( ".block" ).draggable({ grid: [ 10, 10 ] });
-
       $( "div, p, img, .image, .test" ).disableSelection();
-    } );
+} );
 
 
-    var scrollEventHandler = function()
+
+
+(function($){
+
+	$(document).ready(function() {
+		var classes = [ 'style1', 'style2', 'style3', 'style4', 'style5', 'style6' ]; // the classes you want to add
+		$('#chaos').click(function(i) { // the element(s) you want to add the class to.
+			$('#work_1').toggleClass(classes[ Math.floor( Math.random()*classes.length ) ] );
+      $('#work_2').toggleClass(classes[ Math.floor( Math.random()*classes.length ) ] );
+      $('#work_3').toggleClass(classes[ Math.floor( Math.random()*classes.length ) ] );
+      $('#work_4').toggleClass(classes[ Math.floor( Math.random()*classes.length ) ] );
+      $('#work_5').toggleClass(classes[ Math.floor( Math.random()*classes.length ) ] );
+		});
+	});
+
+})(jQuery);
+
+
+var scrollEventHandler = function()
     {
       window.scroll(0, window.pageYOffset)
     }
